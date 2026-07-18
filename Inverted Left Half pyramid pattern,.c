6b57241bc1ca22155977a1872d1aa57abe4da6e9
Tubaislam <sanjidaislam1089@gmail.com>
2026-07-18 09:51:17 +0600

    Update Inverted Left Half pyramid pattern,.c

--- Inverted Left Half pyramid pattern,.c
+++ Inverted Left Half pyramid pattern,.c
@@ -1,16 +1,16 @@
-#include <stdio.h>
+#include<stdio.h>
 
 int main()
 {
     int i,j,k,n;
 
-    for (int i = 0; i<n; i++)
+    for(int i = 0; i<n; i++)
     {
-        for (int j = 0; j < 2 * i; j++)
+        for(int j = 0; j < 2 * i; j++)
         {
             printf(" ");
         }
-        for (int k = 1; k <= 2 * (n- i) - 1; k++)
+        for(int k = 1; k <= 2 * (n- i) - 1; k++)
         {
             printf("%d ", k);
         }
